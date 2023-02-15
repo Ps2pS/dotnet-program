e0c2cba60ef91a112d08e24234df2bd8c867d08b
Pragya Singh <48428695+Ps2pS@users.noreply.github.com>
2023-02-15 17:47:12 +0530

    uploaded files

---
+++ C# Training/Delegate/Delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Me
+{
+    //Delegate declaration
+    public delegate int MathDelegate(int value1, int value2);
+    public delegate void MessageDelegate(int value);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating delegate
+            MathDelegate mathDelegate = new MathDelegate(AddCallback);
+             int total = mathDelegate(30, 20);
+
+            MessageDelegate messageDelegate = MessageCallback;
+            messageDelegate.Invoke(total);
+            Console.ReadKey();
+        }
+
+        static int AddCallback(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        static void MessageCallback(int value)
+        {
+            Console.WriteLine("Total is :", + value);
+        }
+    }
+}
